2a1fa790a08f5aa27ef1f28e04cae7db845e60df
emo8647 <emirhanorak47@gmail.com>
2026-08-24 23:05:13 +0300

    Refactor build.gradle.kts to simplify plugin management
    Removed the buildscript block and adjusted plugin declarations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false // Room/Kapt kullanıyorsan burada da dursun
 }
