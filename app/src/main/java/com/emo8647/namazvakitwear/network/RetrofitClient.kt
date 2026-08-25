6b5921177b3e0581a366139ef5d6eb6dec574658
emo8647 <emirhanorak47@gmail.com>
2026-08-25 11:56:01 +0300

    Implement Retrofit client for Aladhan API

---
+++ app/src/main/java/com/emo8647/namazvakitwear/network/RetrofitClient.kt
@@ -0,0 +1,25 @@
+package com.emo8647.namazvakitwear.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+data class ApiResponse(val data: ApiData)
+data class ApiData(val timings: Map<String, String>)
+
+interface AladhanApi {
+    @GET("v1/timingsByCity?city=Bursa&country=Turkey&method=13")
+    suspend fun getTimings(): ApiResponse
+}
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.aladhan.com/"
+
+    val instance: AladhanApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AladhanApi::class.java)
+    }
+}
